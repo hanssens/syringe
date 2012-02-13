63e21d9f12d801bda7286439e2f3608cd6f30968
J. Hanssens <github@hanssens.org>
2012-02-13 15:44:37 +0100

    Initial checkin of the Syringe project
    Includes basic plumbing and a fair deal of unit tests.

---
+++ Seringe.Tests/SyringeContainerSingletonTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Syringe.Tests
+{
+    [TestClass]
+    public class SyringeContainerSingletonTests
+    {
+        public static SyringeContainer staticContainer { get; set; }
+
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext) 
+        { 
+            // Initialize the Singleton
+            staticContainer = new SyringeContainer();
+            staticContainer.Register<IMathNode, Zero>("zero").AsSingleton();
+        }
+
+        [ClassCleanup()]
+        public static void MyClassCleanup() 
+        { 
+            // TODO: Dispose()
+        }
+
+        [TestMethod]
+        public void AnonymousSingletonRegistration()
+        {
+            //staticContainer.Register<IMathNode, Zero>();
+            var m = staticContainer.Resolve<IMathNode>();
+            Assert.IsInstanceOfType(m, typeof(Zero));
+            Assert.AreEqual(0, m.Calculate());
+        }
+    }
+}
